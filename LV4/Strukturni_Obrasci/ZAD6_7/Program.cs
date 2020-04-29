a27d423eafb8ed7fd29eb518709f1122db3befe9
Kvesss <david.kvesic99@gmail.com>
2020-04-29 20:57:04 +0200

    Task 6-7, Program.cs changed

--- LV4/Strukturni_Obrasci/ZAD6_7/Program.cs
+++ LV4/Strukturni_Obrasci/ZAD6_7/Program.cs
@@ -6,6 +6,25 @@ namespace ZAD6_7
     {
         static void Main(string[] args)
         {
+
+            //Zad 6
+            EmailValidator emailValidator = new EmailValidator();
+            PasswordValidator passwordValidator = new PasswordValidator();
+            string emailCandidate = "visual.com";
+            string passwordCandidate = "abCD56";
+
+            Console.WriteLine(emailValidator.IsValidAddress(emailCandidate));
+            Console.WriteLine(passwordValidator.IsValidPassword(passwordCandidate));
+
+            emailCandidate = "visual@studio.com";
+            passwordCandidate = "abCD5678";
+
+            Console.WriteLine(emailValidator.IsValidAddress(emailCandidate));
+            Console.WriteLine(passwordValidator.IsValidPassword(passwordCandidate));
+
+
+
+            //Zad 7
             RegistrationValidator validator = new RegistrationValidator(5);
             while (true)
             {
@@ -16,7 +35,7 @@ namespace ZAD6_7
                 }
                 Console.WriteLine("Invalid entry!\nTry again!\n");
             }
-            Console.WriteLine("User entry successful!");
+
 
         }
     }
